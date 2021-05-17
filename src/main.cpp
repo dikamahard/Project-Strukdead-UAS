b34826bcf8c679cce5f945bcaef32450a2a9e482
muthiaazzhra <muthiaazzhra@gmail.com>
2021-05-17 20:06:06 +0700

    buat main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,74 @@
-#include <iostream>
+#include<iostream>
 #include "menu.cpp"
-#include "func.cpp"
-#include "object.cpp"
 using namespace std;
 
 
 int main() {
-    mainMenu();
-    
+    ptrdriver pala, coba;
+    ptrpelanggan test;
+    pala = nullptr;
+    int pil; //string nama;
+
+    do {
+        //system ("PAUSE");
+        mainMenu();
+        cout << ">> Pilihan: \n";
+        cin >> pil;
+        switch(pil) {
+
+            case 1 : 
+                menuDriver();
+                cin >> pil;
+                switch(pil) {
+
+                    case 1 : 
+                        menuListDriver(pala);
+                        //harusnya if control disini janagan di fungsi menuPilihDriver
+                        menuPilihDriver(pala);
+                    break;
+
+                    case 2 : 
+                        menuTambahDriver(pala, coba);
+                    break;
+
+                    case 3 : 
+                        menuHapusDriver(pala);
+                    break;
+                }
+            break;
+
+            case 2 :
+                menuPelanggan();
+                cin >> pil;
+                switch(pil) {
+
+                    case 1 : 
+                        menuTambahPelanggan(test, pala);
+                    break;
+
+                    case 2 :
+                        // belom tau, mungkin buat cancel
+                    break;
+                }
+            break;
+
+            case 3 :
+                //buat nampilin driver dan pelanggan semua
+                displayAll(pala);
+                system("PAUSE");
+            break;
+
+            case 4 :
+                cout << ">> Selamat Tinggal \n";
+            break;
+
+            default :
+                std::cout << ">> Pilihan tidak tersedia \n";
+                system("PAUSE");
+            break;
+
+        
+        }
+    }while(pil != 4);
+   
 }
